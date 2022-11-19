77188576b614f0bff34dabb630fefe8679621520
Aamir Razdan <65817609+Aamir-Razdan@users.noreply.github.com>
2022-11-19 14:19:24 +0530

    Add files via upload

---
+++ vowel_consonen.py
@@ -0,0 +1,9 @@
+letter=["a","e","i","o","u","A","E","I","O","U"]
+
+input_letter=input("enter a letter")
+
+if (input_letter in letter):
+    print(input_letter+" is a vowel")
+else:
+    print(input_letter + " is not a vowel")
+    
