dbba846d55f91b97af908557e98eb5121dc0a5ba
Aamir Razdan <65817609+Aamir-Razdan@users.noreply.github.com>
2022-11-21 09:20:47 +0530

    Update vowel_consonen.py

--- vowel_consonen.py
+++ vowel_consonen.py
@@ -1,6 +1,6 @@
 letter=["a","e","i","o","u","A","E","I","O","U"]
 
-input_letter=input("enter a letter")
+input_letter=input("enter any letter")
 
 if (input_letter in letter):
     print(input_letter+" is a vowel")
