fd5dd5e355d0ae341698bc5b10b3ed41afc129aa
Aamir Razdan <65817609+Aamir-Razdan@users.noreply.github.com>
2022-11-21 13:39:59 +0530

    Add files via upload

---
+++ d4positve_negative.py
@@ -0,0 +1,8 @@
+no=int(input("enter a number"))
+
+if(no>=0):
+    print(str(no) +" is a positive number")
+elif(no<0):
+    print(str(no) + " is a negative number")
+else:
+    print(" invalid number")
---
+++ d5even_odd_no.py
@@ -0,0 +1,6 @@
+no=int(input("enter a number"))
+
+if(no%2==0):
+    print(str(no)+" is even number")
+else:
+    print(str(no)+" is a odd number")
